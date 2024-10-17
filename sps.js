d0b50789dcc3e4018bc11d5ace96cd1ca1ab6365
rajnathprasad <rajnathprasad.cs@gmail.com>
2024-10-17 16:52:21 +0530

    configured with local storage

--- sps.js
+++ sps.js
@@ -1,19 +1,17 @@
+var score= JSON.parse(localStorage.getItem("score")) || {tie:0, loss:0, win:0};
+document.querySelector("#scoreOutput").innerText=`Win : ${score.win} Loss : ${score.loss} Tie : ${score.tie}`;
 function generateResponse(){
     let rno=Math.floor((Math.random()*(3-1+1)))+1;
     let options=["Stone","Paper","Scissor"];
     return options[rno-1];
 }
-var score={
-    tie:0,
-    loss:0,
-    win:0
-};
 function check(userInput){
     document.querySelector("#userInput").innerText=`You Chose ${userInput}`;
     let compInput=generateResponse();
     document.querySelector("#compInput").innerText=`Computer Chose ${compInput}`;
     if (userInput == compInput){
         score.tie++;
+        localStorage.setItem("score",JSON.stringify(score));
         document.querySelector("#resultOutput").style.color="grey";
         document.querySelector("#resultOutput").innerText=`Its a Tie`;
         document.querySelector("#scoreOutput").innerText=`Win : ${score.win} Loss : ${score.loss} Tie : ${score.tie}`;
@@ -24,6 +22,7 @@ function check(userInput){
     )
     {
         score.win++;
+        localStorage.setItem("score",JSON.stringify(score));
         document.querySelector("#resultOutput").style.color="green";
         document.querySelector("#resultOutput").innerText=`You won`;
         document.querySelector("#scoreOutput").innerText=`Win : ${score.win} Loss : ${score.loss} Tie : ${score.tie}`;
@@ -32,7 +31,8 @@ function check(userInput){
         userInput === "Paper" && compInput ==="Scissor" ||
         userInput === "Scissor" && compInput ==="Stone")
         {
-        score.loss++;        
+        score.loss++;
+        localStorage.setItem("score",JSON.stringify(score));        
         document.querySelector("#resultOutput").style.color="red";
         document.querySelector("#resultOutput").innerText=`You Lose`;
         document.querySelector("#scoreOutput").innerText=`Win : ${score.win} Loss : ${score.loss} Tie : ${score.tie}`;
@@ -49,5 +49,6 @@ function resetScore(){
     score.loss=0;
     score.win=0;
     score.tie=0;
+    localStorage.setItem("score",JSON.stringify(score));
     document.querySelector("#scoreOutput").innerText=`Win : ${score.win} Loss : ${score.loss} Tie : ${score.tie}`;
 }
